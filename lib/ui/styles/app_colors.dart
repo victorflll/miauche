ac90e57a591e9b9d908fae5049a2f820850cd5db
VictorFulgor <victorluan.lobo@gmail.com>
2021-06-27 20:41:42 -0300

    fixing: erros on app-colors

--- lib/ui/styles/app_colors.dart
+++ lib/ui/styles/app_colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color darkBlue = Color.fromRGBO(39, 11, 111, 255);
-  static const Color violet = Color.fromRGBO(103, 48, 238, 255);
-  static const Color blue = Color.fromRGBO(123, 131, 238, 255);
-  static const Color background = Color.fromRGBO(211, 246, 252, 255);
-  static const Color white = Color.fromRGBO(255, 255, 255, 255);
+  static const Color darkBlue = Color(0xFF280b6f);
+  static const Color violet = Color(0xFF6730ee);
+  static const Color blue = Color(0xFF7b83ee);
+  static const Color background = Color(0xFFd3f6fc);
+  static const Color white = Color(0xFFffffff);
 }
